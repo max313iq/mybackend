45b6c43b2ed28eba941edb3d258c2017315cde6a
max313iq <153689278+max313iq@users.noreply.github.com>
2025-06-08 12:32:59 +0300

    make rate limiter configurable

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -1,7 +1,10 @@
 const rateLimit = require('express-rate-limit');
 
+const WINDOW_MINUTES = process.env.RATE_LIMIT_WINDOW_MINUTES || 15;
+const MAX_REQUESTS = process.env.RATE_LIMIT_MAX || (process.env.NODE_ENV === 'development' ? 1000 : 100);
+
 exports.apiLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: WINDOW_MINUTES * 60 * 1000,
+  max: MAX_REQUESTS,
   message: 'Too many requests from this IP, please try again later.'
 });
